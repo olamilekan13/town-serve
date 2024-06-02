c0504ce413d8f724d14bd88d127191f24a3c600e
Omosanya Olamilekan <olamilekanomosanya@gmail.com>
2024-06-02 19:34:14 +0100

    add functionality to send pdf to the mail

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use App\Mail\PdfMail;
+use Illuminate\Support\Facades\Mail;
 use App\Models\File;
 
 class FileController extends Controller
@@ -23,6 +25,9 @@ class FileController extends Controller
         $file->name = $request->file('pdf_file')->getClientOriginalName();
         $file->path = $path;
         $file->save();
+        // dd($file);
+        Mail::to('olamilekan1312@gmail.com')->send(new PdfMail($file->path));
+
 
         return response()->json(['message' => 'File uploaded successfully']);
     }
---
+++ resources/views/emails/pdf.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PDF Document</title>
+</head>
+<body>
+    <p>Please find the attached PDF document.</p>
+</body>
+</html>
